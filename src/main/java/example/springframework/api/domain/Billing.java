8e6c4dc0fb95e9ccd59c6ac9aa48920a93a07fd4
yerasoni20 <yerasoni20@gmail.com>
2022-04-09 08:29:33 -0400

    Generated Data Model

---
+++ src/main/java/example/springframework/api/domain/Billing.java
@@ -0,0 +1,33 @@
+
+package example.springframework.api.domain;
+
+import example.springframework.api.domain.Card;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Billing implements Serializable
+{
+
+    private Card card;
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+    private final static long serialVersionUID = 7411630398524618582L;
+
+    public Card getCard() {
+        return card;
+    }
+
+    public void setCard(Card card) {
+        this.card = card;
+    }
+
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
