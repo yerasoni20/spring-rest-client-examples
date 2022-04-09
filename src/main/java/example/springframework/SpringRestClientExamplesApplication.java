a9a622c3820bf36f01f0ffe9bcfedb5d890e11b9
yerasoni20 <yerasoni20@gmail.com>
2022-04-09 08:10:29 -0400

    Initial Project Add

---
+++ src/main/java/example/springframework/SpringRestClientExamplesApplication.java
@@ -0,0 +1,12 @@
+package example.springframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestClientExamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestClientExamplesApplication.class, args);
+    }
+}
