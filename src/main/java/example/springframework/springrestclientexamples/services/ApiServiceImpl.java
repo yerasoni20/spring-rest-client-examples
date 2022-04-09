6c98f0d5560767dd69e968afff47b768d26e6d02
yerasoni20 <yerasoni20@gmail.com>
2022-04-09 10:24:04 -0400

    added uri component builder

--- src/main/java/example/springframework/springrestclientexamples/services/ApiServiceImpl.java
+++ src/main/java/example/springframework/springrestclientexamples/services/ApiServiceImpl.java
@@ -1,10 +1,10 @@
 package example.springframework.springrestclientexamples.services;
 
 import example.springframework.api.domain.User;
-import example.springframework.springrestclientexamples.services.ApiService;
-import example.springframework.api.domain.User;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 
@@ -12,18 +12,25 @@ import java.util.List;
  * Created by jt on 9/21/17.
  */
 @Service
-public class ApiServiceImpl implements ApiService {
+public class ApiServiceImpl implements  ApiService {
 
     private RestTemplate restTemplate;
 
-    public ApiServiceImpl(RestTemplate restTemplate) {
+    private final String api_url;
+
+    public ApiServiceImpl(RestTemplate restTemplate, @Value("${api.url}") String api_url) {
         this.restTemplate = restTemplate;
+        this.api_url = api_url;
     }
 
     @Override
     public List<User> getUsers(Integer limit) {
 
-        List<User> userData = restTemplate.getForObject("https://jsonplaceholder.typicode.com/users?_limit=" + limit, List.class);
+        UriComponentsBuilder uriBuilder = UriComponentsBuilder
+                .fromUriString(api_url)
+                .queryParam("_limit", limit);
+
+        List<User> userData = restTemplate.getForObject(uriBuilder.toUriString(), List.class);
         return userData;
     }
 }
