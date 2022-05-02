97c9e1e79c12d0a8b8cb5ecfe617899256fca8db
yerasoni20 <32177481+yerasoni20@users.noreply.github.com>
2022-05-02 16:18:34 -0400

    added rest api service

--- src/main/java/example/springframework/springrestclientexamples/services/ApiService.java
+++ src/main/java/example/springframework/springrestclientexamples/services/ApiService.java
@@ -4,9 +4,7 @@ import example.springframework.api.domain.User;
 
 import java.util.List;
 
-/**
- * Created by jt on 9/21/17.
- */
+
 public interface ApiService {
 
     List<User> getUsers(Integer limit);
